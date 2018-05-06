0c9bac7a4032e8f8d05b451e9f19ab2df3815c11
Iron Brain <wuopcarlos@gmail.com>
2018-05-06 18:20:00 -0400

    set figure plot parameters

--- graph.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-def MakeFig(senal): #Create a function that makes our desired plot
-    plt.ylim(100,120)                                 #Set y min and max values
-    plt.title('Live Streaming from DemoQE')      #Plot the title
-    plt.grid(True)                                  #Turn the grid on
-    plt.ylabel('Amplitud')                            #Set ylabels
-    plt.plot(senal, 'b-', label='Value')       #plot the temperature
-    plt.legend(loc='upper left')                    #plot the legend
