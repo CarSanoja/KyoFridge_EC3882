70785c456a02ce2d24d72ef638928a85c332a735
Iron Brain <wuopcarlos@gmail.com>
2018-05-06 14:15:46 -0400

    Opening a Serial port

--- read_serial.py
+++ read_serial.py
@@ -6,3 +6,22 @@
 import serial
 import time
 
+# Opening a Serial port
+
+# - The port is immediately opened on object creation, when a port is given. It is not opened when
+# port is None and a successive call to open() is required. port is a device name: depending on 
+# operating system. e.g. /dev/ttyUSB0 on GNU/Linux or COM3 on Windows.
+# - The parameter baudrate can be one of the standard values:  50, 75, 110, 134, 150, 200, 300, 600, 1200,
+# 1800, 2400, 4800, 9600, 19200, 38400, 57600, 115200. These are well supported on all platforms.
+# - Standard values above 115200, such as:  230400, 460800, 500000, 576000, 921600, 1000000, 1152000,
+# 1500000, 2000000, 2500000, 3000000, 3500000, 4000000 also work on many platforms and devices.
+# Non-standard values are also supported on some platforms (GNU/Linux, MAC OSX >= Tiger, Windows).
+# - Possible values for the parameter timeout which controls the behavior of read():
+# • timeout = None: wait forever / until requested number of bytes are received
+# • timeout = 0:  non-blocking mode, return immediately in any case, returning zero or more, up to
+# the requested number of bytes
+# • timeout = x:  set timeout to x seconds (float allowed) returns immediately when the requested
+# number of bytes are available, otherwise wait until the timeout expires and return all bytes that were
+# received until then.
+
+DEMOQE_read = serial.Serial('/dev/ttyS1', 19200, timeout=1)
