4e45baea561305b5fd90598b9de81ab567d89e9e
Iron Brain <wuopcarlos@gmail.com>
2018-05-06 13:58:08 -0400

    Documentation for libraries

--- read_serial.py
+++ read_serial.py
@@ -1,2 +1,8 @@
+#The libraries we are going to use are PySerial and Time
+
+# PySerial is encapsulates the access for the serial port.
+# Documentación: https://pythonhosted.org/pyserial/
+
 import serial
 import time
+
