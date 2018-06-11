a7c9b1dd0c7a90921596cd18ca494bb34d61d240
Carlos Sanoja <wuopcarlos@gmail.com>
2018-06-11 00:22:07 -0400

    Deleting useless functions

--- src/entrega 2/Entrega_1_simplified.py
+++ src/entrega 2/Entrega_1_simplified.py
@@ -9,121 +9,12 @@ from matplotlib.lines import Line2D
 import matplotlib.animation as animation
 
 
-# In[76]:
-
-
-# Opening a Serial port
-
-# - The port is immediately opened on object creation, when a port is given. It is not opened when
-# port is None and a successive call to open() is required. port is a device name: depending on 
-# operating system. e.g. /dev/ttyUSB0 on GNU/Linux or COM3 on Windows.
-# - The parameter baudrate can be one of the standard values:  50, 75, 110, 134, 150, 200, 300, 600, 1200,
-# 1800, 2400, 4800, 9600, 19200, 38400, 57600, 115200. These are well supported on all platforms.
-# - Standard values above 115200, such as:  230400, 460800, 500000, 576000, 921600, 1000000, 1152000,
-# 1500000, 2000000, 2500000, 3000000, 3500000, 4000000 also work on many platforms and devices.
-# Non-standard values are also supported on some platforms (GNU/Linux, MAC OSX >= Tiger, Windows).
-# - Possible values for the parameter timeout which controls the behavior of read():
-# .. timeout = None: wait forever until requested number of bytes are received
-# .. timeout = 0:  non-blocking mode, return immediately in any case, returning zero or more, up to
-# the requested number of bytes
-# .. timeout = x:  set timeout to x seconds (float allowed) returns immediately when the requested
-# number of bytes are available, otherwise wait until the timeout expires and return all bytes that were
-# received until then.
-
-
-# In[111]:
-
-
 # VARIABLES para almacenar los valores de los sensores en el tiempo
-#digital_1 = []
-#digital_2 = []
 signal = {'analogico_1': [],'analogico_2': [],'digital_1': [],'digital_2': []}
-#analogico_1 = []
-#signal["analogico_1"].append(0)
-#analogico_2 = []
 # Constantes de manipulacion para el main
-flag_first_run = 0
-flag_trama_len_cst = 0
-list_pos_trama = []
-i = 0
-j = 0
-data_list = []
-#os.remove("Generator_data.txt")
 file = open("Generator_data.txt","w")
 file.write("0")
 file.close()
-
-#Hex to binary
-def dec_to_bin(dec):
-	dec_str = str(int(dec))
-	cod_bin = bin(int(dec_str))
-	return cod_bin
-
-#Binary to decimal
-def bin_to_dec(bina):
-	cod_dec = int(str(bina),2)
-	return cod_dec
-
-# This function make possible to split an integer number to separate the information we want.
-# First we have to translate the hexa code to binary. Second we save every bit following the
-# protocol order:
-# 0 D1 D2 A11 A10 A9 A8 A7
-def decode_channel1_one(number):
-    #decode Channel one
-	part_one_bin = dec_to_bin(number)
-	len_bit_number = len(part_one_bin) -2
-	diferencia = 8 - len_bit_number
-	cero_faltante = "0"*diferencia
-	part_one_bin_comp = (cero_faltante + str(part_one_bin[2:]))
-	digital_one = part_one_bin_comp[1]
-	digital_two = part_one_bin_comp[2]
-	analogic_most_significant = part_one_bin_comp[3:]
-	return digital_one,digital_two,str(analogic_most_significant)
-
-
-# In[115]:
-
-
-# This function make possible to split an integer number to separate the information we want.
-# First we have to translate the hexa code to binary. Second we save every bit following the
-# protocol order:
-# 0 A6 A5 A4 A3 A2 A1 A0
-def decode_channel12_two(number):
-    #decode channe one and two for less significant bits
-	part_one_bin = dec_to_bin(number)
-	len_bit_number = len(part_one_bin) -2
-	diferencia = 8 - len_bit_number
-	cero_faltante = "0"*diferencia
-	part_one_bin_comp = (cero_faltante + str(part_one_bin[2:]))
-	analogic_less_significant = part_one_bin_comp[1:]
-	return str(analogic_less_significant)
-
-
-# In[116]:
-
-
-# This function make possible to split an integer number to separate the information we want.
-# First we have to translate the hexa code to binary. Second we save every bit following the
-# protocol order:
-# 0 0 0 A11 A10 A9 A8 A7
-def decode_channel2_one(number):
-    # decode channel two for most significant bits
-	part_one_bin = dec_to_bin(number)
-	len_bit_number = len(part_one_bin) -2
-	diferencia = 8 - len_bit_number
-	cero_faltante = "0"*diferencia
-	part_one_bin_comp = (cero_faltante + str(part_one_bin[2:]))
-	analogic_most_significant = part_one_bin_comp[3:]
-    
-	return str(analogic_most_significant)
-
-
-# In[117]:
-
-
-# We need to concatened the entirely analogic information.
-def concatenation(string1,string2):
-	return bin_to_dec(int(string1 + str(string2)))
 
 
 def stream(flag_encabezado = 0):
@@ -209,6 +100,22 @@ class Scope(object):
 fig, ax = plt.subplots()
 
 while True:
+	# Opening a Serial port
+	# - The port is immediately opened on object creation, when a port is given. It is not opened when
+	# port is None and a successive call to open() is required. port is a device name: depending on 
+	# operating system. e.g. /dev/ttyUSB0 on GNU/Linux or COM3 on Windows.
+	# - The parameter baudrate can be one of the standard values:  50, 75, 110, 134, 150, 200, 300, 600, 1200,
+	# 1800, 2400, 4800, 9600, 19200, 38400, 57600, 115200. These are well supported on all platforms.
+	# - Standard values above 115200, such as:  230400, 460800, 500000, 576000, 921600, 1000000, 1152000,
+	# 1500000, 2000000, 2500000, 3000000, 3500000, 4000000 also work on many platforms and devices.
+	# Non-standard values are also supported on some platforms (GNU/Linux, MAC OSX >= Tiger, Windows).
+	# - Possible values for the parameter timeout which controls the behavior of read():
+	# .. timeout = None: wait forever until requested number of bytes are received
+	# .. timeout = 0:  non-blocking mode, return immediately in any case, returning zero or more, up to
+	# the requested number of bytes
+	# .. timeout = x:  set timeout to x seconds (float allowed) returns immediately when the requested
+	# number of bytes are available, otherwise wait until the timeout expires and return all bytes that were
+	# received until then.
 	DEMOQE_read = serial.Serial('/dev/ttyUSB0',115200)
 	scope = Scope(ax)
 	ani = animation.FuncAnimation(fig, scope.update, interval=10,blit=True)
