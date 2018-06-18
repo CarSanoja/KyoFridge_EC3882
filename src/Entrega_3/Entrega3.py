cb61514b9788bc3a66df1e6df8373acbcacb97ed
Carlos Sanoja <wuopcarlos@gmail.com>
2018-06-18 16:34:50 -0400

    recibiendo todos los  canales de forma correcta

--- src/Entrega_3/Entrega3.py
+++ src/Entrega_3/Entrega3.py
@@ -18,12 +18,35 @@ def stream(flag_encabezado = 0):
 	#file = open("Generator_data.txt","a")
 	# Lectura del puerto serial
 	DEMOQE_read.flush()
-	data_input_2 = DEMOQE_read.read(3)
+	while True:
+		header = DEMOQE_read.read(1)
+
+		if header[0] == 245:
+			read5= True
+			print('Encabezado F5 OK!!')
+			aux= DEMOQE_read.read(4)
+			data_input_2 = header + aux
+			break
+		elif header[0] == 243:
+			read5 = False
+			#print('Encabezado F3 OK!!')
+			aux = DEMOQE_read.read(2)
+			data_input_2 = header + aux
+			break
+		else:
+			print('no encabezado, leyendo otra vez...')
+			pass
+			
+
 	#print("KRecepcion nueva completa")
 	#print(data_input_2)
 	# ETAPA 1: Se verifica que la trama tenga al encabezado siempre de primero
 	while True:
-		enc_posi = data_input_2.find(245)
+		if (read5 == True):
+			enc_posi = data_input_2.find(245)
+		else:
+			enc_posi = data_input_2.find(243)
+		
 		#print(enc_posi)
 		if enc_posi!=0:
 			data_input_2 = data_input_2[enc_posi:]
@@ -37,8 +60,13 @@ def stream(flag_encabezado = 0):
 			#print("Aqui estaba bien")
 			#print(data_input)
 		for datop in data_input_2:
-			if datop==245:
-				flag_encabezado+=1
+			if read5 == True:
+				if datop==245:
+					flag_encabezado+=1
+			else:
+				if datop==243:
+					flag_encabezado+=1
+
 		if flag_encabezado>1:
 			flag_encabezado=0
 			data_input_2 =  b'\x00' + data_input_2[1:]
@@ -52,7 +80,18 @@ def stream(flag_encabezado = 0):
 	#print(data_input)
     # ETAPA 2: Decodificación del protocolo
 	analogico_1_aux = (((data_input[1] & 31)<<7) + (data_input[2]))*3/4096
+	digital_1_aux = (data_input[1] & 64) >> 6  # entrada digital 1
+	if read5==True:
+		analogico_2_aux = (((data_input[3] & 31)<<7) + (data_input[4]))*3/4096
+		digital_2_aux = (data_input[1] & 32) >> 5 # entrada digital 2
+		signal["analogico_2"].append(analogico_2_aux)
+		signal["digital_2"].append(digital_2_aux)
 	signal["analogico_1"].append(analogico_1_aux)
+	signal["digital_1"].append(digital_1_aux)
+	
+	if read5 == True:
+		print(analogico_2_aux)
+		
 
 def makeFig():
 	# Se verifica la base de tiempo a imprimir como osciloscopio
@@ -61,22 +100,20 @@ def makeFig():
 	plt.ylim(0,3) 
 	plt.title('My Live Streaming Sensor Data')
 	plt.grid(True)                                  			
-	plt.ylabel('Pablonski temp')    
+	plt.ylabel('Amplitud')    
 	plt.xlim(0, t2_read)                       			
 	plt.plot([t*0.0005 for t in range(0,400, 1)],signal["analogico_1"][len(signal["analogico_1"])-400:], 'r-', label='Electret')        
 	plt.legend(loc='upper left')                    				
 
 DEMOQE_read = serial.Serial('/dev/ttyUSB0',115200)
-plt.ion()
+#plt.ion()
 cnt=0
 #seg_div = 1
 while True:
-	stream()
-	cnt=cnt+1
-	if cnt == 400:
-		drawnow(makeFig)                       #Call drawnow to update our live graph
-		plt.pause(.000001)
-		signal["analogico_1"]
-		cnt = 0                     #Pause Briefly. Important to keep drawnow from crashing
-	#print(signal["analogico_1"][len(signal["analogico_1"])-1])
 
+	stream()
+	#cnt=cnt+1
+	#if cnt == 400:
+		#drawnow(makeFig)                       #Call drawnow to update our live graph
+		#plt.pause(.000001)
+	#	cnt = 0                     #Pause Briefly. Important to keep drawnow from crashing
