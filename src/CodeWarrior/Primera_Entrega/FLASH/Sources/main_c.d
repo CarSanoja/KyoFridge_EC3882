525031e8fb5ebce021427332944d379f96b35b96
Unknown <Pabloizquierdo2395@gmail.com>
2018-06-18 18:51:02 -0400

    Añadido código de CW. Trama Variable funcionando
    Cada 4 Segundos se manda el CH2

---
+++ src/CodeWarrior/Primera_Entrega/FLASH/Sources/main_c.d
@@ -0,0 +1,16 @@
+
+Sources/main_c.obj: \
+	C:/Users/user.LC/workspace/Primera_Entrega/Project_Headers/../Sources/main.c \
+	C:/Users/user.LC/workspace/Primera_Entrega/Generated_Code/Cpu.h \
+	C:/Users/user.LC/workspace/Primera_Entrega/Generated_Code/PE_Types.h \
+	C:/Users/user.LC/workspace/Primera_Entrega/Generated_Code/PE_Error.h \
+	C:/Users/user.LC/workspace/Primera_Entrega/Generated_Code/PE_Const.h \
+	C:/Users/user.LC/workspace/Primera_Entrega/Generated_Code/IO_Map.h \
+	C:/Users/user.LC/workspace/Primera_Entrega/Sources/Events.h \
+	C:/Users/user.LC/workspace/Primera_Entrega/Generated_Code/AS1.h \
+	C:/Users/user.LC/workspace/Primera_Entrega/Generated_Code/AD1.h \
+	C:/Users/user.LC/workspace/Primera_Entrega/Generated_Code/TI1.h \
+	C:/Users/user.LC/workspace/Primera_Entrega/Generated_Code/Digital1.h \
+	C:/Users/user.LC/workspace/Primera_Entrega/Generated_Code/Digital2.h \
+	C:/Users/user.LC/workspace/Primera_Entrega/Generated_Code/Muestreo.h 
+
---
+++ src/CodeWarrior/Primera_Entrega/Sources/Events.c
@@ -0,0 +1,214 @@
+/* ###################################################################
+ **     Filename    : Events.c
+**     Project     : led
+**     Processor   : MC9S08QE128CLK
+**     Component   : Events
+**     Version     : Driver 01.02
+**     Compiler    : CodeWarrior HCS08 C Compiler
+**     Date/Time   : 2018-05-05, 18:11, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file Events.c
+** @version 01.02
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+/* MODULE Events */
+
+
+#include "Cpu.h"
+#include "Events.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*
+** ===================================================================
+**     Event       :  AD1_OnEnd (module Events)
+**
+**     Component   :  AD1 [ADC]
+**     Description :
+**         This event is called after the measurement (which consists
+**         of <1 or more conversions>) is/are finished.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void AD1_OnEnd(void)
+{
+  /* Write your code here ... */
+}
+
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnError (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called when a channel error (not the error
+**         returned by a given method) occurs. The errors can be read
+**         using <GetError> method.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnError(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnRxChar (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called after a correct character is received.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled and either the <Receiver>
+**         property is enabled or the <SCI output mode> property (if
+**         supported) is set to Single-wire mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnRxChar(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnTxChar (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called after a character is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnTxChar(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnFullRxBuf (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called when the input buffer is full;
+**         i.e. after reception of the last character 
+**         that was successfully placed into input buffer.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnFullRxBuf(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnFreeTxBuf (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called after the last character in output
+**         buffer is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnFreeTxBuf(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  TI1_OnInterrupt (module Events)
+**
+**     Component   :  TI1 [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void TI1_OnInterrupt(void)
+{
+	/* Write your code here ... */
+	if (trigger == TRUE){
+		cnt = cnt + 1;
+	}
+	if (estado==WAIT){
+		estado=MEDIR;
+	}
+
+}
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnSwINT (module Events)
+**
+**     Component   :  Cpu [MC9S08QE128_80]
+**     Description :
+**         This event is called when the SWI interrupt had occurred.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cpu_OnSwINT(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  KB1_OnInterrupt (module Events)
+**
+**     Component   :  KB1 [KBI]
+**     Description :
+**         This event is called when the active signal edge/level
+**         occurs. This event is enabled only if <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+/* END Events */
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.09]
+**     for the Freescale HCS08 series of microcontrollers.
+**
+** ###################################################################
+*/
