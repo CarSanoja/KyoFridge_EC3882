d4c9a8526b8db2f25e8d465e3ba1fbbec7e831b4
Carlos Sanoja <wuopcarlos@gmail.com>
2018-07-10 09:15:17 -0400

    changes
    Se añaden los parametros que llegan a la funcion para evitar leer desde archivos
    se modifica el rango de parametros en el ue funciona el analisis de histogramas

--- src/Entrega_3/Caso_Separado/sonido.py
+++ src/Entrega_3/Caso_Separado/sonido.py
@@ -6,23 +6,19 @@ import os
 import json
 import numpy as np
 
-def sonido_analisis():
-	with open('data_im.txt') as json_file:  
-			condicional = json.load(json_file)
 
-	with open('data.json') as json_file:  
-			signal = json.load(json_file)
 
+def sonido_analisis(signal,condicional):
 	electret = signal["analogico_1"]
-
 	if condicional == 0:
 		pass
 	else:
 		electret_np = np.asarray(electret)
 		hist, bin_edges = np.histogram(electret_np)
+		print(np.histogram(electret_np))
 		maxi = hist.max()
 		maxi_histo = np.asscalar(maxi)
-		if maxi_histo > 850: 
+		if maxi_histo > 350: 
 			return True
 		else:
 			return False
